126e4c4c113b93e895a464d5d2ab90032e8db537
Ronny Hansen <RonnyHan@gmail.com>
2019-04-25 01:38:52 +0200

    added script to creater Jager tracing backend to cluster

---
+++ release/create-jaeger.sh
@@ -0,0 +1,3 @@
+# https://github.com/jaegertracing/jaeger-kubernetes
+
+kubectl create -f https://raw.githubusercontent.com/jaegertracing/jaeger-kubernetes/master/all-in-one/jaeger-all-in-one-template.yml
